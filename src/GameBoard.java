4879ae19e87151295aa66cfd7f6fafd1278ffec0
shrestha24 <67696266+shrestha24@users.noreply.github.com>
2023-03-19 11:32:43 +0530

    GameBoard complete

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -22,9 +22,44 @@ public class GameBoard {
         while (nextTurn.size() > 1) {
             Player player = nextTurn.poll();
 
-            int currentPosition = playersCurrentPosition.get(player.getplayerName());
+            int currentPosition = playersCurrentPosition.get(player.getPlayerName());
             int diceValue = dice.rollDice();
-            
+            int nextCell = currentPosition + diceValue;
+            if (nextCell > boardSize) nextTurn.offer(player);
+            else if (nextCell == boardSize){
+                System.out.println(player.getPlayerName() + "won the game");
+            }
+            else {
+                int[] nextPosition = new int[1];
+                boolean[] b = new boolean[1];
+                nextPosition[0] = nextCell;
+                snakes.forEach(v-> {
+                    if (v.startPoint == nextCell){
+                        nextPosition[0] = v.endPoint;
+                    }
+                });
+
+                if (nextPosition[0] != nextCell)
+                    System.out.println(player.getPlayerName() + "Biter by snake at: "+ nextCell);
+                ladders.forEach(v->{
+                    if (v.startPoint == nextCell) {
+                        nextPosition[0] = v.endPoint;
+                        b[0] = true;
+                    }
+                });
+
+                if (nextPosition[0] != nextCell && b[0])
+                    System.out.println(player.getPlayerName()+ "Got ladder at:" + nextCell);
+                if (nextPosition[0] == boardSize){
+                    System.out.println(player.getPlayerName() + "won the game" );
+                }
+                else {
+                    playersCurrentPosition.put(player.getPlayerName(), nextPosition[0]);
+                    System.out.println(player.getPlayerName()+ "is at position" +nextPosition[0]);
+                    nextTurn.offer(player);
+                }
+             }
+
         }
     }
 }
--- src/Player.java
+++ src/Player.java
@@ -1,9 +1,29 @@
+
+
+
 public class Player {
     private String playerName;
     private int id;
 
 
-     Player(String playerName, int id) {
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    Player(String playerName, int id) {
         this.playerName = playerName;
         this.id = id;
     }
